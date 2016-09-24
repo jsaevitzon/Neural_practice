b25a2514bce6bbec542b28f7d13f7fd93f467d29
Jaryd Saevitzon <jsaevitzon@g.ucla.edu>
2016-09-23 18:46:08 -0700

    first commit

---
+++ chap1.py
@@ -0,0 +1,21 @@
+import math
+
+
+def sigFunc(z):
+    e=2.71828
+    return 1/(1+math.pow(e,-z))
+
+
+# w denotes collection of all weights in the network
+# b denotes all the biases
+# n is the total number of training inputs
+# a is the vector of outputs from the network when x is input
+# 
+def quadCostFunc(w, b):
+
+
+    
+
+
+
+
